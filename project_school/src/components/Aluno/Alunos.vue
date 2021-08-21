bf07d23d990896c86b86d236d6d3194d1bb53469
carl0sR0ma0 <cvpromao@gmail.com>
2021-08-21 15:33:41 -0300

    First Commit of the Project

---
+++ project_school/src/components/Aluno/Alunos.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <Titulo text="Alunos" />
+    <div>
+      <input
+        type="text"
+        placeholder="Nome do Aluno"
+        v-model="nome"
+        @keyup.enter="addAluno()"
+      />
+      <button class="btn btnInput" @click="addAluno()">Adicionar</button>
+    </div>
+
+    <table>
+      <thead>
+        <th>Mat</th>
+        <th>Nome</th>
+        <th>Opções</th>
+      </thead>
+      <tbody v-if="alunos.length">
+        <tr v-for="(aluno, index) in alunos" :key="index">
+          <td>{{ aluno.id }}</td>
+          <td>{{ aluno.nome }} {{ aluno.sobrenome }}</td>
+          <td>
+            <button class="btn btnDanger" @click="remove(aluno)">
+              Remover
+            </button>
+          </td>
+        </tr>
+      </tbody>
+      <tfoot v-else>
+        Nenhum Aluno Encontrado
+      </tfoot>
+    </table>
+  </div>
+</template>
+
+<script>
+import Titulo from "../shared/Titulo.vue";
+export default {
+  components: {
+    Titulo,
+  },
+  data() {
+    return {
+      titulo: "Aluno",
+      nome: "",
+      alunos: [],
+    };
+  },
+  created() {
+    this.$http
+      .get("http://localhost:3000/alunos")
+      .then((res) => res.json())
+      .then((alunos) => (this.alunos = alunos));
+  },
+  props: {},
+  methods: {
+    addAluno() {
+      let _aluno = {
+        nome: this.nome,
+        sobrenome: "",
+      };
+
+      this.$http
+        .post("http://localhost:3000/alunos", _aluno)
+        .then((res) => res.json())
+        .then((aluno) => {
+          this.alunos.push(aluno);
+          this.nome = "";
+        });
+    },
+    remove(aluno) {
+      this.$http.delete(`http://localhost:3000/alunos/${aluno.id}`).then(() => {
+        let indice = this.alunos.indexOf(aluno);
+        this.alunos.splice(indice, 1);
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+input {
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  color: #687f7f;
+  display: inline;
+  outline: 0;
+}
+
+.btnInput {
+  border: 0px;
+  padding: 20px;
+  font-size: 1.3em;
+  display: inline;
+  background-color: rgb(116, 115, 115);
+}
+
+.btnInput:hover {
+  padding: 20px;
+  margin: 0;
+  border: 0;
+}
+</style>
